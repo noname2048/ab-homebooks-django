55e845b283118093c9f738a91f88f0f859657e0a
noname2048 <sungwook.csw@gmail.com>
2021-05-07 23:33:50 +0900

    books, bookshelves added

---
+++ homebooks/books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from bookshelves.models import Bookshelf
+
+
+class Book(models.Model):
+
+    name = models.CharField(
+        _("name"),
+        max_length=100,
+    )
+
+    long_name = models.CharField(
+        _("long_name"),
+        max_length=200,
+    )
+
+    isbn = models.SlugField(
+        _("isbn"),
+        max_length=50,
+        allow_unicode=False,
+    )
+
+    bookshelf = models.ForeignKey(
+        Bookshelf,
+        on_delete=models.deletion.CASCADE,
+    )
---
+++ homebooks/bookshelves/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+user_model = get_user_model()
+
+
+class Bookshelf(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(
+        user_model,
+        on_delete=models.deletion.PROTECT,
+        blank=True,
+    )
+
+    name = models.CharField(
+        _("name"),
+        max_length=100,
+        blank=True,
+    )
