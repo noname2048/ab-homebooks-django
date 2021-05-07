a4a2321dcca7ec583c08a8a1fd24b3d36113ebd5
noname2048 <sungwook.csw@gmail.com>
2021-05-07 17:49:51 +0900

    wsgi, asgi -> prod

--- homebooks/homebooks/wsgi.py
+++ homebooks/homebooks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homebooks.settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "homebooks.settings.prod")
 
 application = get_wsgi_application()
