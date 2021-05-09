af3e54de0e9fcc124fa5c38c3ddd3837b69fd90d
noname2048 <sungwook.csw@gmail.com>
2021-05-09 20:39:14 +0900

    accounts added admin custom for (new bookshelf is has none)

--- homebooks/accounts/admin.py
+++ homebooks/accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.utils.html import mark_safe
+from django.template.defaultfilters import truncatechars
 
+from bookshelves.models import Bookshelf
 
 User = get_user_model()
 
@@ -13,8 +15,24 @@ class UserAdmin(admin.ModelAdmin):
         "username",
         "email",
         "is_staff",
-        "password",
+        "short_password",
+        "have_bookshelf",
     ]
+
+    actions = ["make_bookshelf"]
+
+    def short_password(self, obj):
+        return truncatechars(obj.password, 10)
 
     def avatar_url_image(self, obj):
         return mark_safe(f'<img src={obj.avatar_url} "width=30px" height="30px" />')
+
+    def have_bookshelf(self, obj):
+        return obj.bookshelf_set.exists()
+
+    @admin.action(description="Make BookShelf, if have not.")
+    def make_bookshelf(modelamin, request, queryset):
+        users = queryset.all()
+        for user in users:
+            if user.bookshelf_set.exists() == False:
+                user.bookshelf_set.create(name="default")
