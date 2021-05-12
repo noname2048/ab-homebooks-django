89429472d147c5a3e12d50903c38aad4f57b271e
noname2048 <sungwook.csw@gmail.com>
2021-05-12 22:54:05 +0900

    new: restframework-jwt added
    user needs to login to see home
    
    none

--- homebooks/homebooks/settings/dev.py
+++ homebooks/homebooks/settings/dev.py
@@ -84,3 +84,10 @@ DATABASES = {
         "NAME": os.environ["SERVER_DEFAULT_DB_DBNAME"],
     }
 }
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+    ),
+}
