69adc3a0fecdeb1b3dc27fcfff5db0d4eadffe6a
noname2048 <sungwook.csw@gmail.com>
2021-06-14 23:00:06 +0900

    fix(docker-compose): replace app root (homebooks -> repo root)

--- compose/base/build.sh
+++ compose/base/build.sh
@@ -1,2 +1,2 @@
-docker build -f ./base.Dockerfile -t noname2048/ab_python_base:1.0 .
+docker build -f ./base.Dockerfile -t noname2048/ab_python_base:1.0 ./../../
 docker push noname2048/ab_python_base:1.0
