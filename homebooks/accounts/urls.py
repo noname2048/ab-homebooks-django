34f79c72572b2a724a4e0a48987128021cca1e7c
noname2048 <sungwook.csw@gmail.com>
2021-06-16 18:28:34 +0900

    chore(poetry): djangorestframework-jwt -> djangorestframework-simplejwt
    change
    djangorestframework-jwt (rest_framework_jwt)
    to
    djangorestframework-simplejwt (rest_framewokr_simplejwt)

--- homebooks/accounts/urls.py
+++ homebooks/accounts/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from django_pydenticon.views import image as pydenticon_image
 
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
-from accounts.views import discover_existence_from_email
+# from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from .views import discover_existence_from_email
 
 
 urlpatterns = [
-    path(
-        "pydenticon/image/<path:data>.png", pydenticon_image, name="pydenticon_image"
-    ),  # pydention
-    path("token-auth/", obtain_jwt_token),  # login
-    path("token-auth/refresh/", refresh_jwt_token),  # refresh
+    path("pydenticon/<path:data>.png", pydenticon_image, name="pydenticon"),  # pydention
+    # path("token-auth/", obtain_jwt_token),  # login
+    # path("token-auth/refresh/", refresh_jwt_token),  # refresh
     path("email", discover_existence_from_email, name="discorver_by_email"),
+    path("api/token/", TokenRefreshView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
