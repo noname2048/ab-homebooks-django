fdad67526f1e7f2ffb04532e8ebe72991e655dff
noname2048 <sungwook.csw@gmail.com>
2021-06-18 20:31:48 +0900

    chore(black): add space between classes

--- homebooks/accounts/forms.py
+++ homebooks/accounts/forms.py
@@ -3,19 +3,18 @@ from django.core.validators import EmailValidator
 from django.contrib.auth import forms as auth_forms, get_user
 from django.contrib.auth import get_user_model
 
+
 class AccountEmailForm(forms.Form):
     email = forms.EmailField(help_text="user_email", validators=[EmailValidator()])
 
-class AccountSignupForm(auth_forms.UserCreationForm):
 
+class AccountSignupForm(auth_forms.UserCreationForm):
     class Meta(auth_forms.UserChangeForm.Meta):
         model = get_user_model()
-        fields = ("email", )
+        fields = ("email",)
+
 
 class NoPasswordSignupForm(forms.Form):
-
     class Meta:
         model = get_user_model()
         fields = ["email", "name"]
-
-        
