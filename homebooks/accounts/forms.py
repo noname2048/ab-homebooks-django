7aa43f97d5ff38814d25bd470b310d3272ab7ac1
noname2048 <sungwook.csw@gmail.com>
2021-06-20 20:31:49 +0900

    test(signup): signup forms practice(repeat)

--- homebooks/accounts/forms.py
+++ homebooks/accounts/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.core.validators import EmailValidator
 from django.contrib.auth import forms as auth_forms, get_user
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+UserModel = get_user_model()
 
 
 class AccountEmailForm(forms.Form):
@@ -14,7 +17,32 @@ class AccountSignupForm(auth_forms.UserCreationForm):
         fields = ("email",)
 
 
-class NoPasswordSignupForm(forms.Form):
-    class Meta:
-        model = get_user_model()
-        fields = ["email", "name"]
+class NoPasswordSignupForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = UserModel
+        fields = (
+            "email",
+            "name",
+        )
+
+
+from django.core.exceptions import ValidationError
+
+
+class DjangoCustomSignupForm(forms.Form):
+
+    email = forms.EmailField(required=True)
+    name = forms.CharField(required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(), strip=False)
+    password2 = forms.CharField(widget=forms.PasswordInput(), strip=False)
+
+    error_messages = {"password_mismatch": "The two password fields didn't match"}
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"], code="password_mismatch"
+            )
+        return password2
--- homebooks/accounts/views.py
+++ homebooks/accounts/views.py
@@ -70,3 +70,73 @@ class SignupAPIView(APIView):
             tb = traceback.format_exc()
         except KeyError:
             return JsonResponse({"message": "Key_error"}, status=400)
+
+
+from django.views import generic
+from .forms import DjangoCustomSignupForm
+
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import hashers
+
+
+@require_http_methods(["GET", "POST"])
+def django_custom_signup_function_view(request: HttpRequest):
+    if request.method == "GET":
+        form = DjangoCustomSignupForm()
+        return render(
+            request,
+            "accounts/empty.html",
+            {
+                "form": form,
+            },
+        )
+
+    if request.method == "POST":
+        form = DjangoCustomSignupForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            user = User(
+                email=data["email"],
+                name=data["name"],
+            )
+            user.set_password(data["password"])
+            user.save(commit=True)
+
+            return JsonResponse(
+                {
+                    "message": "user creation request accepted",
+                    "data": {
+                        "email": user.email,
+                        "name": user.name,
+                    },
+                },
+                status=201,
+            )
+
+        return JsonResponse(form.errors, status=400)
+
+
+class DjangoCustomSignupClassView(generic.View):
+    welcome_message = "welcome"
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        form = DjangoCustomSignupForm()
+        data = form.a
+        data["message"] = self.welcome_message
+        return JsonResponse(form.cleaned_data)
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        form = DjangoCustomSignupForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            data["message"] = self.welcome_message
+            return JsonResponse(data)
+
+
+from django.views import generic
+
+
+class DjangoCustomSignupCreateView(generic.CreateView):
+    pass
