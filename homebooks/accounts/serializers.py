8e41f3ffd4a957ba5896ed25e2165932fc2fc230
noname2048 <sungwook.csw@gmail.com>
2021-06-21 22:53:35 +0900

    test(serializer): add signup serializer

--- homebooks/accounts/serializers.py
+++ homebooks/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, Serializer
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 User = get_user_model()
 
@@ -14,3 +15,52 @@ class UserSerializer(serializers.ModelSerializer):
 class SignupSerializer(serializers.Serializer):
     email = serializers.EmailField()
     name = serializers.CharField()
+    password1 = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text="type backup password",
+        style={"input_type": "password", "placeholder": "password"},
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text="type backup password",
+        style={"input_type": "password", "placeholder": "password"},
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "full_name",
+        )
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data.get("password1"))
+        return super().create(validated_data)
+
+
+class SignupModelSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text="type backup password",
+        style={"input_type": "password", "placeholder": "password"},
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text="type backup password",
+        style={"input_type": "password", "placeholder": "password"},
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "full_name",
+        )
+
+    def create(self, validated_data):
+        validated_data["password"] = make_password(validated_data.get("password1"))
+        return super().create(validated_data)
