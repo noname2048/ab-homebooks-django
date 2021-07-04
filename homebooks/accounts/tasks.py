560254a483d8a31bf6bdb8b83f2ad800ed72a636
noname2048 <sungwook.csw@gmail.com>
2021-07-04 20:57:53 +0900

    feat: add sendgrid api mail, single sender authentication

--- homebooks/accounts/tasks.py
+++ homebooks/accounts/tasks.py
@@ -1,5 +1,8 @@
-from celery import shared_task
+from celery import shared_task, task
 from .models import User
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 @shared_task
@@ -28,3 +31,15 @@ def user_isexist(email):
     if user is not None:
         return True
     return False
+
+
+def send_welcome_mail(user: User):
+    mail = Mail(
+        from_email="sungwook.csw@noname2048.dev",
+        to_emails=user.email,
+        subject="welcome!",
+        html_content="welcome email test",
+    )
+    sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
+    response = sg.client.mail.send.post(request_body=mail.get())
+    return response
