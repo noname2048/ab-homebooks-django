8c9f8cb2aa4c743612be36e0947f44aba8420fb1
noname2048 <sungwook.csw@gmail.com>
2021-07-05 22:19:38 +0900

    feat(celery): prepare token email send. (domain email authenticate with sendgrid)

--- homebooks/accounts/tasks.py
+++ homebooks/accounts/tasks.py
@@ -34,13 +34,30 @@ def user_isexist(email):
     return False
 
 
-@celery_app.task(serializer="json")
-def send_welcome_mail(user: User):
+@celery_app.task
+def send_welcome_mail(email: str):
     mail = Mail(
         from_email="sungwook.csw@noname2048.dev",
-        to_emails=user.email,
+        to_emails=email,
         subject="welcome!",
         html_content="welcome email test",
+    )
+    sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
+    response = sg.client.mail.send.post(request_body=mail.get())
+    return response
+
+
+@celery_app.task
+def send_token_email(email: str, token=None):
+
+    if token is None:
+        token = ""
+
+    mail = Mail(
+        from_email="noreply@noname2048.dev",
+        to_emails=email,
+        subject="homebooks login link",
+        html_content=f"to login, click here http://localhost:8000/login/?token={token}",
     )
     sg = SendGridAPIClient(os.environ["SENDGRID_API_KEY"])
     response = sg.client.mail.send.post(request_body=mail.get())
