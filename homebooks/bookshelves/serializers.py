81cf3d2f6e937b76fa93bd80925cef1a47b2c544
noname2048 <sungwook.csw@gmail.com>
2021-07-13 03:39:45 +0900

    feat(bookshelves): work serializers with viewsets

--- homebooks/bookshelves/serializers.py
+++ homebooks/bookshelves/serializers.py
@@ -1,12 +1,27 @@
+import logging
+
 from rest_framework.serializers import ModelSerializer
 
 from bookshelves.models import Bookshelf
 from accounts.serializers import UserSerializer
 
+import logging
+
+logger = logging.getLogger("bookshelves")
+
 
 class BookshelfSerializer(ModelSerializer):
-    user = UserSerializer(read_only=True)
+    # user = UserSerializer()
 
     class Meta:
         model = Bookshelf
         fields = ["user", "name"]
+
+    def create(self, validated_data):
+        logger.warning("serializer create works")
+        bookshelf = Bookshelf(**validated_data)
+        request = self.context.get("request", None)
+        if request and bookshelf.user is None:
+            bookshelf.user = request.user.id
+        bookshelf.save()
+        return bookshelf
--- homebooks/bookshelves/views.py
+++ homebooks/bookshelves/views.py
@@ -1,11 +1,19 @@
+import rest_framework.status
 from django.shortcuts import render
 
 from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
+from rest_framework.request import Request
 
 
 from bookshelves.models import Bookshelf
 from bookshelves.serializers import BookshelfSerializer
+from .forms import BookshelfForm
+from accounts.serializers import UserSerializer
+
+import logging
+
+logger = logging.getLogger("bookshelves")
 
 
 class MyBookshelfViewSet(ModelViewSet):
@@ -17,8 +25,23 @@ class MyBookshelfViewSet(ModelViewSet):
         context["request"] = self.request
         return context
 
-    def list(self, request):
-        pass
+    def get_queryset(self):
+        return self.request.user.bookshelf_set.all()
 
-    def create(self, request):
-        pass
+    def list(self, request: Request, *args, **kwargs) -> Response:
+        queryset = self.get_queryset()
+        serializer = BookshelfSerializer(queryset, many=True)
+        return Response(serializer.data, status=rest_framework.status.HTTP_200_OK)
+
+    def create(self, request: Request, *args, **kwargs) -> Response:
+        data = {"user": request.user.id, "name": request.POST.get("name")}
+        serializer = BookshelfSerializer(data=data)
+
+        logger.warning("warning")
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            ret = serializer.errors
+            ret["data"] = serializer.data
+            return Response(ret)
