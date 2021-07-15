4509c169dcbcd0e69f3a71d7d700e09e99466517
noname2048 <sungwook.csw@gmail.com>
2021-07-15 23:10:27 +0900

    improve(book): improve create view

--- homebooks/books/views.py
+++ homebooks/books/views.py
@@ -10,11 +10,12 @@ from .serializers import *
 from rest_framework import viewsets
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from .tasks import request_book_info
 
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
-    serializer_class = BookSerializer
+    serializer_class = BookInfoSerializer
     permission_classes = [permissions.AllowAny]
 
     @swagger_auto_schema(
@@ -38,19 +39,32 @@ class BookListView(generics.ListAPIView):
         if name:
             queryset = queryset.filter(name__icontains="name")
 
-        serializer = BookSerializer(queryset, many=True)
+        serializer = BookInfoSerializer(queryset, many=True)
         return Response(
             {"data": serializer.data, "name": name}, status=rest_framework.status.HTTP_200_OK
         )
 
 
 class BookCreateView(generics.CreateAPIView):
-    def create(self, request):
-        # 오로지 ISBN만으로 등록
-        isbn = request.POST.get("isbn")
-        # TODO: isbn 검색요청을 MSA에 요청. 있을경우 그 정보로 등록
+    def create(self, request: Request, *args, **kwargs) -> Response:
 
-        pass
+        isbn = request.POST.get("isbn")
+        if len(isbn) != 13:
+            return Response(
+                {"message": "잘못된 isbn 요청입니다."}, status=rest_framework.status.HTTP_400_BAD_REQUEST
+            )
+        queryset = Book.objects.filter(isbn=isbn)
+        if queryset:
+            return Response(
+                {"message": "book alreaady exists"},
+                status=rest_framework.status.HTTP_400_BAD_REQUEST,
+            )
+        else:
+            data = {"isbn": isbn, "bookshelf": request.user.bookshelf_set.first.id}
+            serializer = UserBookRegisterSerializer(data=data)
+            serializer.save()
+
+            return Response(serializer.data, status=rest_framework.status.HTTP_201_CREATED)
 
 
 book_list = BookListView.as_view()
