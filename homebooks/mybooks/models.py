5a82f9f12b9a1858339985fac8105489bb5852ab
noname2048 <sungwook.csw@gmail.com>
2021-08-01 21:05:37 +0900

    fix(model): delete other auto field, change related_name, add index

--- homebooks/mybooks/models.py
+++ homebooks/mybooks/models.py
@@ -5,20 +5,24 @@ from homebooks.models import TimeStampedModel
 
 
 class Bookshelf(TimeStampedModel):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.deletion.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.deletion.CASCADE, related_name="mybookshelf"
+    )
     title = models.CharField(max_length=100)
     color = models.CharField(max_length=10)
 
 
 class Book(TimeStampedModel):
-    bookshelf = models.ForeignKey("Bookshelf", on_delete=models.deletion.CASCADE)
-    isbn = models.BigAutoField()
+    bookshelf = models.ForeignKey(
+        "Bookshelf", on_delete=models.deletion.CASCADE, related_name="mybooks"
+    )
+    isbn = models.PositiveBigIntegerField()
     origin = models.ForeignKey("OriginalBook", on_delete=models.deletion.CASCADE, null=True)
     have = models.BooleanField(blank=True, default=False)
 
 
 class OriginalBook(TimeStampedModel):
-    isbn = models.BigAutoField(primary_key=True)
+    isbn = models.PositiveBigIntegerField(primary_key=True, db_index=True)
     title = models.CharField(max_length=100)
     thumbnail = models.ImageField()
 
